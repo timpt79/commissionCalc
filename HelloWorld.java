1b4519bdf5fc61a477bfdbdb47486cf2d44504ca
timpt79 <timpt79@gmail.com>
2014-01-16 13:33:30 -0700

    Hello world example

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import hello.Hello;
+
+/**
+ *
+ * @author Tim
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        Hello helloObject = new Hello();
+
+        helloObject.sayHello();
+    }//this executes the method say hello
+}//this is the main class
